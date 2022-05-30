26ab8e794780a00b219fdcd081b91281a24c4349
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-30 16:30:19 +0300

    added stylization

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,33 @@
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
+import NavBar from './NavBar';
 
 function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Activity[]>('http://localhost:5000/api/activities')
       .then(res => {
         setActivities(res.data)
       })
   }, [])
 
   return (
-    <div className="App">
-      <Header as='h2' icon='users' content='Reactivities' />
-      <List>
-        {activities.map(a => (
-          <List.Item key={a.id}>
-            {a.title}
-          </List.Item>
-        ))}
-      </List>
-    </div>
+    <Fragment>
+      <NavBar />
+      <Container style={{ marginTop: '7em' }}>
+        <List>
+          {activities.map(a => (
+            <List.Item key={a.id}>
+              {a.title}
+            </List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
   );
 }
 
