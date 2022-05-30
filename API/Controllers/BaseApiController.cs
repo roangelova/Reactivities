adbc3da8bc33e67e46c98c3b0f862b5b2e88c704
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-30 12:39:38 +0300

    added Mediator to BaseController

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -10,6 +11,12 @@ namespace API.Controllers
     [ApiController]
     public class BaseApiController : Controller
     {
+
+        private IMediator _mediator;
+
+        protected IMediator Mediator => _mediator ??=
+            HttpContext.RequestServices.GetService<IMediator>();
+
 
     }
 }
