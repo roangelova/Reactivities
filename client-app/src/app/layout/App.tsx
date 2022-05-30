0a36c5965d52cd4ab3e5bf9a0b0a08a287b84bfe
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-30 16:14:05 +0300

    structure amended and Activity interface added

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from 'react';
-import './App.css';
 import axios from 'axios';
 import { Header, List } from 'semantic-ui-react';
+import { Activity } from '../models/activity';
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios.get('http://localhost:5000/api/activities')
@@ -18,7 +18,7 @@ function App() {
     <div className="App">
       <Header as='h2' icon='users' content='Reactivities' />
       <List>
-        {activities.map((a: any) => (
+        {activities.map(a => (
           <List.Item key={a.id}>
             {a.title}
           </List.Item>
