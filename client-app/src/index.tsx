e12f85a5a7c0cfcfd60e811c243b67481e2e323b
Roslava Angelova <88967550+roangelova@users.noreply.github.com>
2022-05-30 11:19:13 +0300

    added semantic ui lib

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +9,6 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-    <App />
+  <App />
 );
 reportWebVitals();
